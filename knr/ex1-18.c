ba46697c9ac3d2967e25f2f1be26f1e7a52f269c
fruitnuke <dan@amygda.la>
2016-05-14 12:19:30 -0400

    A better implementation for ex1-18 (remove trailing blanks).

--- knr/ex1-18.c
+++ knr/ex1-18.c
@@ -1,5 +1,5 @@
 /* Write a program to remove trailing blanks and tabs from each line
- * of input, and to delete entirely blank linkes */
+ * of input, and to delete entirely blank lines */
 
 #include <stdio.h>
 
@@ -8,44 +8,65 @@
 #define OUT 0
 #define IN 1
 
+
+int getln(char s[], int max);
+int length(char s[]);
+void strip(char s[]);
+
+
 main()
 {
-    int c;
-    int state;
-    int start;
-    int i;
     char line[MAXLN];
 
-    while (1)
-    {
-        for (i = 0, start = 0; i < MAXLN - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-            line[i] = c;
-
-            if (c == ' ' || c == '\t') {
-                if (state == OUT) {
-                    state = IN;
-                    start = i;
-                }
-            }
-            else
-                state = OUT;
-        }
-
-        if (state == IN)
-            i = start;
-
-        if (c == '\n' && i > 0) {
-            line[i] = c;
-            ++i;
-        }
-
-        line[i] = '\0';
-
-        printf("%s", line);
-
-        if (c == EOF)
-            break;
+    while (getln(line, MAXLN) > 0) {
+        strip(line);
+        if (line[0] != '\0')
+            printf("%s\n", line);
     }
 
     return 0;
+}
+
+/* Strip trailing blanks and tabs and newlines, in place. */
+void
+strip(char s[])
+{
+    int i;
+
+    for (i = length(s) - 1; i >= 0; i--)
+       /* The character tests can presumably be replaced with
+        * isspace(), from the standard library. */
+        if (s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
+            break;
+
+    /* This works on empty lines and all whitespace lines as the loop
+     * terminates with i = -1. */
+    s[i+1] = '\0';
+}
+
+/* Find the length of the string */
+int
+length(char s[])
+{
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+        ;
+    return i;
+}
+
+int
+getln(char s[], int lim)
+{
+    int c, i;
+
+    i = 0;
+    while (lim-- > 0 && (c = getchar()) != EOF && c != '\n')
+        s[i++] = c;
+
+    if (c == '\n')
+        s[i++] = c;
+
+    s[i] = '\0';
+    return i;
 }
